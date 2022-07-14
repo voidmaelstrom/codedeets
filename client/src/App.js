697a4045847c87b153952e2577d6f7f436a22465
Ryan Blake <xbungalowx@gmail.com>
2022-07-13 21:16:59 -0500

    navbar MVP finished, added MUI icons dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 import './App.css';
 
 import Navbar from './components/navbar/Navbar';
@@ -9,13 +5,7 @@ import Navbar from './components/navbar/Navbar';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Navbar/>
-=======
-      <header className="App-header">
-        <h1>Welcome to Codedeets</h1>
-      </header>
->>>>>>> main
+        <Navbar/>
     </div>
   );
 }
