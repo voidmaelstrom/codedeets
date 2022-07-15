95cf724bab6cb2ad74b88ea7e949b0040be2b89e
Ryan Blake <xbungalowx@gmail.com>
2022-07-14 21:43:14 -0500

    added comments

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 
@@ -5,16 +6,21 @@ const Form = () => {
   // a local state to store the currently selected file.
   const [selectedFile, setSelectedFile] = React.useState(null);
 
+ 
+
+
   const handleSubmit = async (event) => {
     event.preventDefault()
     const formData = new FormData();
-    formData.append("file", selectedFile, 'test.md');
+    if(selectedFile){
+        formData.append("file", selectedFile, 'test.md');
+    }
     //formData.append('tag', 'javascript')
     console.log(selectedFile)
     
     try {
       const response = await fetch("http://localhost:5050/posts", {
-        method: "post",
+        method: "POST",
         headers: { "Content-Type": "multipart/form-data"},
         body: formData
       });
@@ -22,13 +28,13 @@ const Form = () => {
       console.log(error)
     }
   }
-
+  //Possibly use filereader, read as arraybuffer
   const handleFileSelect = (event) => {
     setSelectedFile(event.target.files[0])
   }
 
   return (
-    <form onSubmit={handleSubmit} action="/" method='post'>
+    <form onSubmit={handleSubmit} action="/posts" method='POST'>
       <input type="file" onChange={handleFileSelect}/>
       <input type="submit" value="Upload File" />
     </form>
