1138b6ee1f6c8187701d3a836fade2732d56c9c1
wildfirez98 <james@areafamily.us>
2022-07-16 15:04:14 -0500

    More routing and page work

--- client/src/App.js
+++ client/src/App.js
@@ -4,37 +4,54 @@ import Navbar from './Components/Navbar/Navbar'
 import PostContainer from './Components/PostContainer/PostContainer';
 import UserList from './Components/UserList/UserList';
 import GettingStarted from './Components/GettingStarted/GettingStarted';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 
 import Form from './Components/Form/Form';
 //import MarkdownPostContainer from './components/MarkdownPostContainer/MarkdownPostContainer';
 
-      
+
 const App = () => {
 
-let [data, setData] = useState([null])
+  let [data, setData] = useState([null])
 
-// General set up for API call
-useEffect(() => {
-  const fetchData = async () => {
-    const response = await fetch('http://localhost:5000/posts')
-    const resData = await response.json()
-    console.log(resData)
-    setData(resData)
-  }
-  fetchData()
-},[])
+  // General set up for API call
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:5000/posts')
+      const resData = await response.json()
+      console.log(resData)
+      setData(resData)
+    }
+    fetchData()
+  }, [])
 
 
 
   return (
+
     <div className="container">
-      <div className="navbar"><Navbar /></div>
-      {/*Added form here simply for ease of file uploading testing  */}
-      <Form></Form>
-      <div className="users"><h1>User List</h1><UserList /></div>
-      <div className="posts"><h1>Recent Posts</h1><PostContainer /></div>
-      <div className="footer">Footer</div>
+      <Router>
+        <Navbar />
+        {/*Added form here simply for ease of file uploading testing  */}
+        {/* <Form></Form> */}
+        <div className="users"><h1>User List</h1><UserList /></div>
+        {/* <div className="posts"><h1>Recent Posts</h1><PostContainer /></div> */}
+
+        {/* <- Where the route's component will render */}
+        <div className="display">
+          <Routes>
+            <Route path="/" element={<PostContainer />} />
+            <Route path="/gettingstarted" element={<GettingStarted />} />
+
+          </Routes>
+        </div>
+
+        <div className="footer">Footer</div>
+      </Router>
     </div>
+
+
+
   );
 }
 
