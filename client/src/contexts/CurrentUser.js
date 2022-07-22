a55be46f60e481f8b30535f5e02af44d0da17314
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-07-22 17:14:35 -0600

    Address 404 errors due to additional call to /auth that is not needed

--- client/src/contexts/CurrentUser.js
+++ client/src/contexts/CurrentUser.js
@@ -7,21 +7,21 @@ function CurrentUserProvider({ children }) {
 
     useEffect(() => {
 
-        const getNewUser = async () => {
-            try{
-                await axios({
-                    method: "post",
-                    url: "http://localhost:5000/auth",
-                    data: currentUser
-                })
-                .then(response => {
-                    setCurrentUser(response.data.user)
-                    localStorage.setItem('token', response.data.token)
-                })
-            }catch(err){
-                console.log(err)
-            }
-        }
+        // const getNewUser = async () => {
+        //     try{
+        //         await axios({
+        //             method: "post",
+        //             url: "http://localhost:5000/auth",
+        //             data: currentUser
+        //         })
+        //         .then(response => {
+        //             setCurrentUser(response.data.user)
+        //             localStorage.setItem('token', response.data.token)
+        //         })
+        //     }catch(err){
+        //         console.log(err)
+        //     }
+        // }
 
         const getLoggedInUser = async () => {
             let response = await fetch('http://localhost:5000/auth/profile', {
@@ -34,9 +34,10 @@ function CurrentUserProvider({ children }) {
         }
         if(localStorage.getItem('token')){
             getLoggedInUser()
-        }else{
-            getNewUser()
         }
+        // }else{
+        //     getNewUser()
+        // }
 
     }, [currentUser])
 
